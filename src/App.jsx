f1b041d4035c509b3f868c250e89c2b664081b2c
TechByKen <diala.kenneth98@gmail.com>
2025-07-07 11:40:32 +0100

    Styled theapp

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,27 @@ import { useState } from "react"
 function App() {
   const [city, setCity] = useState('');
  const [weatherData, setWeatherData] = useState(null);
+ const [isLoading, setIsLoading] = useState(false);
 
 const fetchWeatherData = async () => {
   const API_KEY = '52cffd9aae7d459ea4683134250707'
   const API_ENDPOINT = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`;
 
   try {
+    setIsLoading(true)
    const response = await fetch(API_ENDPOINT);
    const data = await response.json()
+
+   if (data.error) {
+  alert(data.error.message);
+  setWeatherData(null);
+  
+}else{
+
+  setWeatherData(data)
+}
    
-   setWeatherData(data)
-   console.log(data)
+   
   }catch(error){
     console.error('Error fetching weather data:', error)
   }
@@ -24,7 +34,7 @@ const fetchWeatherData = async () => {
   return (
   <div className="bg-blue-600 h-screen flex flex-col items-center justify-center">
     <div className="bg-white mx-10 p-9 rounded shadow-md mb-6">
-      <h1 className="font-bold mb-4 text-2xl">Weather App</h1>
+      <h1 className="font-bold mb-4 text-2xl">Weather Application</h1>
       <input type="text" placeholder="Enter city name" value={city} className="border rounded p-2 w-full mb-4" onChange={(e) => setCity(e.target.value)}/>
       <button className="bg-blue-600 cursor-pointer p-2 hover:bg-blue-400 transition text-white rounded" onClick={fetchWeatherData}>Get Weather</button>
     </div>
